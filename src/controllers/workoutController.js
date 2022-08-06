93be760f7322e39b2b8fa4c09495ec5fbcb74bcd
Zamir Narro <josue.narro.a@gmail.com>
2022-08-06 14:42:33 -0500

    Add workout service and DB

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require('../services/workoutService')
+
 const getAllWorkouts = (req, res) => {
-  res.send("Get all workouts")
+  const allWorkouts = workoutService.getAllWorkouts()
+  res.send({ status: "OK", data: allWorkouts })
 }
 
 const getOneWorkout = (req, res) => {
+  const workout = workoutService.getOneWorkout(req.params.workoutId);
   res.send("Get an existing workout")
 }
 
 const createNewWorkout = (req, res) => {
+  const createdWorkout = workoutService.createNewWorkout(req.body);
   res.send("Create a new workout")
 }
 
 const updateOneWorkout = (req, res) => {
+  const updatedWorkout = workoutService.updateOneWorkout(req.params.id, req.body);
   res.send("Update an existing workout")
 }
 
 const deleteOneWorkout = (req, res) => {
+  workoutService.deleteOneWorkout(req.params.workoutId);
   res.send("Delete an existing workout")
 }
 
