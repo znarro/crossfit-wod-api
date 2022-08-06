5516c9744d68faf68ae4de5f756662ff5a40783a
Zamir Narro <josue.narro.a@gmail.com>
2022-08-06 17:56:44 -0500

    Create endpoint working

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -2,26 +2,46 @@ const workoutService = require('../services/workoutService')
 
 const getAllWorkouts = (req, res) => {
   const allWorkouts = workoutService.getAllWorkouts()
-  res.send({ status: "OK", data: allWorkouts })
+  res.status(200).send({ status: "OK", data: allWorkouts })
 }
 
 const getOneWorkout = (req, res) => {
-  const workout = workoutService.getOneWorkout(req.params.workoutId);
+  const workout = workoutService.getOneWorkout(req.params.workoutId)
   res.send("Get an existing workout")
 }
 
 const createNewWorkout = (req, res) => {
-  const createdWorkout = workoutService.createNewWorkout(req.body);
-  res.send("Create a new workout")
+  const { body } = req
+
+  if (
+    !body.name ||
+    !body.mode ||
+    !body.equipment ||
+    !body.exercises ||
+    !body.trainerTips
+  ) {
+    return
+  }
+
+  const newWorkout = {
+    name: body.name,
+    mode: body.mode,
+    equipment: body.equipment,
+    exercises: body.exercises,
+    trainerTips: body.trainerTips,
+  }
+
+  const createdWorkout = workoutService.createNewWorkout(newWorkout)
+  res.status(201).send({ status: "OK", data: createdWorkout })
 }
 
 const updateOneWorkout = (req, res) => {
-  const updatedWorkout = workoutService.updateOneWorkout(req.params.id, req.body);
+  const updatedWorkout = workoutService.updateOneWorkout(req.params.id, req.body)
   res.send("Update an existing workout")
 }
 
 const deleteOneWorkout = (req, res) => {
-  workoutService.deleteOneWorkout(req.params.workoutId);
+  workoutService.deleteOneWorkout(req.params.workoutId)
   res.send("Delete an existing workout")
 }
 
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,3 +1,4 @@
+const { v4: uuid } = require('uuid')
 const Workout = require('../database/Workout')
 
 const getAllWorkouts = () => {
@@ -9,8 +10,15 @@ const getOneWorkout = () => {
   return
 }
 
-const createNewWorkout = () => {
-  return
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" })
+  }
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert)
+  return createdWorkout
 }
 
 const updateOneWorkout = () => {
