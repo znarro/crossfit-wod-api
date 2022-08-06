8e898f3c12589c42629d2dd9852a002a08bd5677
Zamir Narro <josue.narro.a@gmail.com>
2022-08-06 18:36:36 -0500

    Complete CRUD operations

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -2,17 +2,21 @@ const workoutService = require('../services/workoutService')
 
 const getAllWorkouts = (req, res) => {
   const allWorkouts = workoutService.getAllWorkouts()
-  res.status(200).send({ status: "OK", data: allWorkouts })
+  res.send({ status: "OK", data: allWorkouts })
 }
 
 const getOneWorkout = (req, res) => {
-  const workout = workoutService.getOneWorkout(req.params.workoutId)
-  res.send("Get an existing workout")
+  const { params: { workoutId } } = req
+
+  if (!workoutId) return
+
+  const workout = workoutService.getOneWorkout(workoutId)
+  res.send({ status: "OK", data: workout })
 }
 
 const createNewWorkout = (req, res) => {
   const { body } = req
-
+  
   if (
     !body.name ||
     !body.mode ||
@@ -22,7 +26,7 @@ const createNewWorkout = (req, res) => {
   ) {
     return
   }
-
+  
   const newWorkout = {
     name: body.name,
     mode: body.mode,
@@ -30,19 +34,27 @@ const createNewWorkout = (req, res) => {
     exercises: body.exercises,
     trainerTips: body.trainerTips,
   }
-
+  
   const createdWorkout = workoutService.createNewWorkout(newWorkout)
   res.status(201).send({ status: "OK", data: createdWorkout })
 }
 
 const updateOneWorkout = (req, res) => {
-  const updatedWorkout = workoutService.updateOneWorkout(req.params.id, req.body)
-  res.send("Update an existing workout")
+  const { body, params: { workoutId } } = req
+  
+  if (!workoutId) return
+  
+  const updatedWorkout = workoutService.updateOneWorkout(workoutId, body)
+  res.send({ status: "OK", data: updatedWorkout })
 }
 
 const deleteOneWorkout = (req, res) => {
-  workoutService.deleteOneWorkout(req.params.workoutId)
-  res.send("Delete an existing workout")
+  const { params: { workoutId } } = req
+  
+  if (!workoutId) return
+  
+  workoutService.deleteOneWorkout(workoutId)
+  res.status(204).send({ status: "OK" })
 }
 
 module.exports = {
--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -6,8 +6,9 @@ const getAllWorkouts = () => {
   return allWorkouts
 }
 
-const getOneWorkout = () => {
-  return
+const getOneWorkout = (workoutId) => {
+  const workout = Workout.getOneWorkout(workoutId)
+  return workout
 }
 
 const createNewWorkout = (newWorkout) => {
@@ -17,16 +18,18 @@ const createNewWorkout = (newWorkout) => {
     createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
     updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" })
   }
+
   const createdWorkout = Workout.createNewWorkout(workoutToInsert)
   return createdWorkout
 }
 
-const updateOneWorkout = () => {
-  return
+const updateOneWorkout = (workoutId, changes) => {
+  const updatedWorkout = Workout.updateOneWorkout(workoutId, changes)
+  return updatedWorkout
 }
 
-const deleteOneWorkout = () => {
-  return
+const deleteOneWorkout = (workoutId) => {
+  Workout.deleteOneWorkout(workoutId)
 }
 
 module.exports = {
