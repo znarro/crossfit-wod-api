2aeed8a8f450ed3b634e889010195323c6032804
Zamir Narro <josue.narro.a@gmail.com>
2022-08-06 13:53:46 -0500

    Add workouts controller

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const workoutController = require('../../controllers/workoutController')
+
+const router = express.Router()
+
+router
+  .get('/', workoutController.getAllWorkouts)
+  .get('/:workoutId', workoutController.getOneWorkout)
+  .post("/", workoutController.createNewWorkout)
+  .patch("/:workoutId", workoutController.updateOneWorkout)
+  .delete("/:workoutId", workoutController.deleteOneWorkout)
+
+module.exports = router
