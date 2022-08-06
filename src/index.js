c089e1a4619ca05947465ab98f402d1d8de4cb63
Zamir Narro <josue.narro.a@gmail.com>
2022-08-06 13:26:22 -0500

    Add versioning

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const v1Router = require('./v1/routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('<h2>Working!</h2>')
-})
+app.use('/api/v1', v1Router)
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`) 
